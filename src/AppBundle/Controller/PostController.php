3f2403e6e65bd1af613bf76a3aac6c1a1c27a8db
Trajche <roskoskitrajce@gmail.com>
2017-11-11 21:00:32 +0100

    created post form

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -6,6 +6,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PostController extends Controller
 {
@@ -19,7 +24,7 @@ class PostController extends Controller
     }
     
     /**
-     * @Route("/post/{id}")
+     * @Route("/post/{id}", requirements={"id":"\d+"})
      */
     public function postAction(Request $request, $id)
     {
@@ -32,12 +37,48 @@ class PostController extends Controller
      */
     public function createPostAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/default.html.twig');
+        $post = new Post();
+        
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class)
+            ->add('summary', TextareaType::class)
+            ->add('content', TextareaType::class)
+            ->add('tags', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Post'))
+            ->getForm();
+        $form->handleRequest($request);
+                
+        if ($form->isSubmitted() && $form->isValid()) {
+            
+            //get the author id
+            $author_id = $user = $this->get('security.token_storage')->getToken()->getUser()->getId();
+
+            //look for the author in the db based on his id
+            $rep = $this->getDoctrine()->getRepository('AppBundle:User');
+            $author = $rep->find($author_id);
+            
+            //set the author
+            $post->setAuthor($author);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($post);
+            $em->flush();
+            
+            $this->addFlash(
+                'notice',
+                'Succesfully added a new post'
+            );
+            return $this->redirectToRoute('homepage');
+        }
+        
+        return $this->render('post.html.twig', array(
+            'title' => 'Create a new Post',
+            'form' => $form->createView()
+            ));
     }
     
     /**
-     * @Route("/post/{id}/edit", name="edit_post")
+     * @Route("/post/{id}/edit", name="edit_post", requirements={"id":"\d+"})
      */
     public function editPostAction(Request $request, $id)
     {
@@ -46,7 +87,7 @@ class PostController extends Controller
     }
     
     /**
-     * @Route("/post/{id}/delete", name="delete_post")
+     * @Route("/post/{id}/delete", name="delete_post", requirements={"id":"\d+"})
      */
     public function deletePostAction(Request $request, $id)
     {
