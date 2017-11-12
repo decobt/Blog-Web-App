15b280be905c7aa31471cbf9b97ce90501c97231
Trajche <roskoskitrajce@gmail.com>
2017-11-12 22:12:13 +0100

    added Comment Entity

---
+++ src/AppBundle/Entity/Comment.php
@@ -0,0 +1,15 @@
+<?php
+
+// src/AppBundle/Entity/Comment.php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_comments")
+ */
+
+class Comment{
+    
+}
